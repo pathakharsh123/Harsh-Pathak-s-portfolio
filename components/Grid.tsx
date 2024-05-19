2ceda0acdb7bf7dd2b9bceb189ad655c2cadba08
pathakharsh123 <112664461+pathakharsh123@users.noreply.github.com>
2024-05-20 00:06:57 +0530

    created the bento grid and trying to fix its error

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/bento-grid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id='about'>
+        <BentoGrid>
+            {gridItems.map(({id,title,description,className,img,imgClassName,titleClassName,spareImg})=>(
+                <BentoGridItem
+                    id= {id}
+                    key= {id}
+                    title = {title}
+                    description={description}
+                    className={className}
+                    img= {img}
+                    imgClassName= {imgClassName}
+                    titleClassName  = {titleClassName}
+                    spareimg=  {spareImg}
+                />
+            ))}
+        </BentoGrid>
+    </section>
   )
 }
 
